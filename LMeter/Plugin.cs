b097f4e963918f9b51e031b89e39ce515f55c912
Yuna Clover <yunaclover@outlook.com>
2026-05-03 13:17:06 +0800

    修改版本号

--- LMeter/Plugin.cs
+++ LMeter/Plugin.cs
@@ -15,7 +15,7 @@ namespace LMeter
     {
         public const string ConfigFileName = "LMeter.json";
 
-        public static string Version { get; private set; } = "0.4.4.4";
+        public static string Version { get; private set; } = "0.4.4.5";
         public static string ConfigFileDir { get; private set; } = "";
         public static string ConfigFilePath { get; private set; } = "";
         public static string AssemblyFileDir { get; private set; } = "";
